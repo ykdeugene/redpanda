6331a052dc98f55659a4ee8273dbfe27b82eb4d4
ykdeugene <ykdeugene@gmail.com>
2023-05-22 08:56:36 +0800

    first commit

---
+++ src/main/java/com/tmsjsb/redpanda/RedpandaApplication.java
@@ -0,0 +1,13 @@
+package com.tmsjsb.redpanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedpandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedpandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmsjsb/redpanda/RedpandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmsjsb.redpanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedpandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
