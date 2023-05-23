d68e5e6733b7048814709b23f6a216059c139f1a
Calvin Ho <calvinhoka@hotmail.com>
2023-05-23 09:05:54 +0800

    Create SampleController.java
    added skeleton

---
+++ src/main/java/com/tmsjsb/redpanda/Controller/SampleController.java
@@ -0,0 +1,28 @@
+package com.tmsjsb.redpanda.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleController {
+
+  @GetMapping("/{index}")
+  public String simpleHello(@PathVariable int index) {
+
+    return somearray(index);
+  }
+
+  private static String somearray(int index) {
+    List<String> people = new ArrayList<>();
+
+    people.add("Mike");
+    people.add("Adam");
+    people.add("Craig");
+
+    System.out.println(people.toString());
+    return people.get(index).toString();
+  }
+  
+}
