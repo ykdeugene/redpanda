5659e728d5812402a483fcf98de4e5207479f387
vlazybones <eylinbxy@gmail.com>
2023-05-30 14:18:49 +0800

    updated @Pattern annotation for user entity - rebase 30/05

--- src/main/java/com/tmsjsb/redpanda/Entity/UserEntity.java
+++ src/main/java/com/tmsjsb/redpanda/Entity/UserEntity.java
@@ -6,19 +6,26 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "user")
 public class UserEntity {
 
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Pattern(regexp = "^[a-zA-Z0-9]+$")
+  @NotBlank
   private String username;
 
   @Column(name = "password")
+  @NotBlank
   private String password;
 
   @Column(name = "email")
+  @Email
   private String email;
 
   @Column(name = "activestatus")
@@ -27,28 +34,33 @@ public class UserEntity {
   public String getUsername() {
     return username;
   }
+
   public String getPassword() {
     return password;
   }
+
   public String getEmail() {
     return email;
   }
+
   public int getActiveStatus() {
     return activeStatus;
   }
 
-  public void setUsername(String username){
+  public void setUsername(String username) {
     this.username = username;
   }
-  public void setPassword(String password){
+
+  public void setPassword(String password) {
     this.password = password;
   }
-  public void setEmail(String email){
+
+  public void setEmail(String email) {
     this.email = email;
   }
-  public void setActiveStatus(int activeStatus){
+
+  public void setActiveStatus(int activeStatus) {
     this.activeStatus = activeStatus;
   }
 
 }
-
