e10be4db515a072b82d80ac16841fff7e55753af
rockangelz <calvinhoka@hotmail.com>
2023-06-14 20:34:14 +0800

    update intercept partially

--- src/main/java/com/tmsjsb/redpanda/Controller/InterceptorController.java
+++ src/main/java/com/tmsjsb/redpanda/Controller/InterceptorController.java
@@ -1,10 +1,14 @@
 package com.tmsjsb.redpanda.Controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tmsjsb.redpanda.Service.AuthService;
 import com.tmsjsb.redpanda.Service.ErrorMgrService;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -28,15 +32,67 @@ public class InterceptorController {
     this.authService = authService;
   }
 
-  @Pointcut("execution(* com.tmsjsb.redpanda.Controller.AdminController..*(..)) && !execution(* com.tmsjsb.redpanda.Controller.AuthController.*(..))")
 
+  @Pointcut("execution(* com.tmsjsb.redpanda.Controller.AdminController..*(..)) && !execution(* com.tmsjsb.redpanda.Controller.AuthController.AuthLogin(..)) && args(.., *)")
   public void interceptedAdminMethods() {}
 
   @Around("interceptedAdminMethods()")
   public Object interceptedAdminMethods(ProceedingJoinPoint joinPointAdmin) throws Throwable {
     Map<String, Object> errorObject = new HashMap<>();
+    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
 
-    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+    Object[] args = joinPointAdmin.getArgs();
+    Object body = args[0];
+    System.out.println("Request Body Object: " + body);
+
+    String password = null;
+    String taskid = null;
+    String app_acronym = null;
+    String plan_app_acronym = null;
+
+    if (body instanceof Map) {
+      Map<?, ?> requestBody = (Map<?, ?>) body;
+
+      for (Map.Entry<?, ?> entry : requestBody.entrySet()) {
+        Object key = entry.getKey();
+        Object value = entry.getValue();
+
+        if ("password".equals(key)) {
+          password = value.toString();
+          System.out.println("Password: " + password);
+        }
+        else if ("taskid".equals(key)) {
+          taskid = value.toString();
+          System.out.println("taskid: " + taskid);
+        }
+        else if ("app_acronym".equals(key)) {
+          app_acronym = value.toString();
+          System.out.println("app_acronym: " + app_acronym);
+        }
+        else if ("plan_app_acronym".equals(key)) {
+          plan_app_acronym = value.toString();
+          System.out.println("plan_app_acronym: " + plan_app_acronym);
+        }
+    }
+  }
+
+  //   String requestBody = extractRequestBody(request);
+  //   // add logic here for getting permits from app selected
+  //   System.out.println("requestbody" + requestBody);
+  //   if (controllerName.contains("task")) {
+  //     // check taskid for app concat -> task permits
+  //     System.out.println("Executing method in TaskController.");
+  // } else if (controllerName.contains("app")) {
+  //     // check for app_acronym -> app permits
+  //     System.out.println("Executing method in AppController.");
+  // } else if (controllerName.contains("plan")) {
+  //     // check for plan_app_acronym -> plan permits
+  //     System.out.println("Executing method in PlanController.");
+  // } else {
+  //     // Logic for other controllers
+  //     System.out.println("Executing method in " + controllerName + " controller.");
+  // }
+
     String jwt = request.getHeader("Authorization");
     String ipAddress = request.getRemoteAddr();
     String browserType = request.getHeader("User-Agent");
@@ -97,5 +153,14 @@ public class InterceptorController {
       return joinPoint.proceed();
     }
   }
+
+  private String extractRequestBody(HttpServletRequest request) throws IOException {
+    // Extract the request body from the request object
+    // Implement the logic to read the request body based on your specific requirements
+    // For example, you can use a library like Jackson or Gson to deserialize the request body into an object
+    // In this example, let's assume you are extracting the request body as a String
+    // Adapt this method based on your use case
+    // Here, we are simply returning the request body as a String
+    return request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+  }
 }
-  
