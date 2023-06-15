1a737afda935b50e4fc0e33c76a3d1d66da0b3b8
vlazybones <eylinbxy@gmail.com>
2023-06-15 09:11:22 +0800

    @NotBlank updated in entity

--- src/main/java/com/tmsjsb/redpanda/Entity/PlanEntity.java
+++ src/main/java/com/tmsjsb/redpanda/Entity/PlanEntity.java
@@ -5,6 +5,7 @@ import lombok.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -18,20 +19,25 @@ public class PlanEntity {
   @Id
   @Column(name = "plan_mvp_name")
   @JsonProperty("Plan_MVP_name")
+  @NotBlank
   private String plan_MVP_name;
 
   @Id
   @Column(name = "plan_appacronym")
   @JsonProperty("Plan_appAcronym")
+  @NotBlank
   private String planappAcronym;
 
   @Column(name = "plan_startdate")
+  @NotBlank
   private String PlanstartDate;
 
   @Column(name = "plan_enddate")
+  @NotBlank
   private String PlanendDate;
 
   @Column(name = "plan_colour")
+  @NotBlank
   private String Plancolour;
 
 }
